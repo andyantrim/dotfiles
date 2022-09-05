254c9f1209a26775a0cbec60b0aec8ab6bbae797
Andy Blair <andyantrim@gmail.com>
2022-09-05 10:52:08 +0100

    SQL config

--- setup.bash
+++ setup.bash
@@ -1,5 +1,14 @@
+echo "Prepending .bak to old files"
+mv ~/.bashrc ~/.bashrc.bak
+mv ~/.tmux.conf ~/.tmux.conf.bak
+mv ~/.config/nvim ~/.config/nvim.bak
 
+echo "Adding dot files"
 
 ln -s $PWD/nvim/ ~/.config/nvim
 ln -s $PWD/.bashrc ~/.bashrc
 ln -s $PWD/.tmux.conf ~/.tmux.conf
+
+echo "Adding vim-plug to NeoVim"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
